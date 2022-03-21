5e7bc57546d116e92c051a57471530040d80af7b
Plaksunova Anhelina <AngelinaPlax@gmail.com>
2022-03-21 22:34:33 +0300

    feat: complete task 22

--- task22.js
+++ task22.js
@@ -11,3 +11,36 @@
 // transform ([1, 3, '--double-next', 4]) => [1, 3, 4, 4]
 
 // transform ([1, 3, '--discard-prev', 4]) => [1, 4]
+
+function transform(arr) {
+  if (arr.includes('--discard-next')) {
+    let ind = arr.indexOf('--discard-next');
+    let newArr = [];
+    arr.forEach((item, index) => {
+      if (index != ind && index != ind + 1) return newArr.push(item);
+    });
+    return newArr;
+  } else if (arr.includes('--discard-prev')) {
+    let ind = arr.indexOf('--discard-prev');
+    let newArr = [];
+    arr.forEach((item, index) => {
+      if (index != ind && index != ind - 1) return newArr.push(item);
+    });
+    return newArr;
+  } else if (arr.includes('--double-next')) {
+    let ind = arr.indexOf('--double-next');
+    return arr.map((item, index) => {
+      if (index != ind) {
+        return item;
+      } else return (arr[ind] = arr[ind + 1]);
+    });
+  } else if (arr.includes('--double-prev')) {
+    let ind = arr.indexOf('--double-prev');
+    return arr.map((item, index) => {
+      if (index != ind) {
+        return item;
+      } else return (arr[ind] = arr[ind - 1]);
+    });
+  }
+}
+console.log(transform([1, 3, '--double-prev', 4]));
